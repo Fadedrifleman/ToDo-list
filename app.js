94cbebf92454d1551e369eebbca6ce595e597c78
fadedrifleman <fadedrifleman@gmail.com>
2022-04-24 15:00:16 +0530

    master

--- app.js
+++ app.js
@@ -3,14 +3,39 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
     var today = new Date();
-
-    if (today.getDay() === 6 || today.getDay() === 0){
-        res.send("Weekend");   
-    } else {
-        res.send("weekday");
+    var day = "";
+    var a = today.getDay();
+    switch (a) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("error:" + a);
+            break;
     }
+    res.render("list", {kindOfDay: day});  
 });
 
 
