e051e5294660e7779e91ec13c1176e9b2c2b8f7e
fadedrifleman <fadedrifleman@gmail.com>
2022-04-25 12:51:19 +0530

    .

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static("public")); 
+app.use(express.static("public"));
 
 let items = [];
+let workItems = [];
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -23,21 +24,35 @@ app.get("/", (req, res) => {
 
     let day = today.toLocaleDateString("en-US", option);
     res.render("list", {
-        Day: day,
+        listTitle: day,
         newListItems: items
     });
 });
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+}); 
 
-    items.push(item);
-    res.redirect("/");
+app.get("/work", (req, res) => {
+    res.render("list", {
+        listTitle: "Work",
+        newListItems: workItems
+    })
+}) 
+
+app.post("/work", (req, res) => {
+    let item = req.body.newItem;
+
+    workItems.push(item);
+    res.redirect("/work");
 });
-
-
-
-
 
 
 app.listen(3000, () => {
